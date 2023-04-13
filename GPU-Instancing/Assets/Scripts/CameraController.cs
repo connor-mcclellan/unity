3a886b374bd621a8d4dc5238242f4a378f5b2aee
mccbc <cmcclellan1010@gmail.com>
2023-04-12 22:59:24 -0400

    working isometric camera rotation about a fixed point

--- GPU-Instancing/Assets/Scripts/CameraController.cs
+++ GPU-Instancing/Assets/Scripts/CameraController.cs
@@ -6,14 +6,12 @@ public class CameraController : MonoBehaviour
 {
     public Transform pivotPoint; // Pivot point for camera rotation
 
-    private float targetRotation; // Target rotation for smoothing
-    private float startTime = 0f;
+    private float targetRotation = 0f; // Target rotation for smoothing
+    private float startTime = -2f;
     private Vector3 offset;
 
     void Start()
     {
-        // Set initial target rotation to current rotation
-        targetRotation = transform.rotation.eulerAngles.y;
         offset = transform.position - pivotPoint.transform.position;
     }
 
@@ -30,10 +28,16 @@ public class CameraController : MonoBehaviour
             targetRotation -= 90f;
             startTime = Time.time;
         }
-        if (Time.time - startTime < 2f)
+        if ((Time.time - startTime < 2f) & (Mathf.Abs(targetRotation) > 0.00001f))
         {
-            transform.RotateAround(pivotPoint.transform.position, Vector3.up, targetRotation * (Time.time - startTime)/2f);
-            targetRotation -= targetRotation * (Time.time - startTime)/2f;
+            transform.RotateAround(pivotPoint.transform.position, Vector3.up, targetRotation * (Time.time - startTime) / 2f);
+            targetRotation -= targetRotation * (Time.time - startTime) / 2f;
+        }
+        else if (Mathf.Abs(targetRotation) < 0.00001f) 
+        {
+            transform.RotateAround(pivotPoint.transform.position, Vector3.up, targetRotation);
+            startTime = -2.0f;
+            targetRotation = 0.0f;
         }
         //transform.position = offset + pivotPoint.transform.position;
     }
